a23463c43bb3eebaddbfd460b3426cbc766aff48
Артур Галеев <a.galeev@tapir.ws>
2020-09-30 00:36:26 +0500

    xz

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,8 +4,17 @@ const errorHandler = require('../utils/errorHandler')
 
 module.exports.getAll = async function (req, res) {
   try {
-    // const genres = Genre.find({});
-    const movies = await Movie.find().limit(10).skip(0)
+    const movies = await Movie.find();
+    movies.forEach(
+      async function (newMovie) {
+        let genreArr = []
+        await Genre.find( { "_id": { $in: newMovie.genre_ids }  } ).lean().exec(function (err, docs) {
+          genreArr = docs;
+        });
+        newMovie.genre_ids = genreArr;
+      }
+    )
+
     res.status(200).json(movies)
   } catch (e) {
     errorHandler(res, e)
